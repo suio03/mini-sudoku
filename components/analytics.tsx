db047c29a7b7d24bab9f3ee6b1ffe20f1ccb7d30
banjo <mr.singledog@gmail.com>
2025-10-03 13:50:39 +1000

    add analytics

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -2,41 +2,9 @@ import Script from 'next/script'
 
 const Analytics = () => (
     <>
-        {/* google analytics */}
-        <Script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-QGXPBKHEYR"
-        />
-
-
-        <Script
-            id="google-analytics"
-            dangerouslySetInnerHTML={{
-                __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'G-QGXPBKHEYR');
-                `,
-            }}
-        />
-        <Script
-            id="microsoft-clarity"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-                __html: `
-                (function(c,l,a,r,i,t,y){
-                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "s8n7tucvbf");
-        `
-            }}
-        />
         <Script
             defer
-            data-domain="tangogameunlimited.app"
+            data-domain="minisudoku.net"
             src="https://actone.app/js/script.js"
         />
 
